8edd56e105f9c07c21307589cb11aacd7d8d8ff7
Darkista <loualex128@gmail.com>
2023-06-28 07:31:35 -0400

    V1.0

---
+++ README.txt
@@ -0,0 +1,9 @@
+This program is made for sending images through whatsapp with an automatic crop
+
+Dependencies:
+pywhatkit
+OpenCV
+tkinter
+os
+
+Author: Loualex128
--- src/main.py
+++ src/main.py
@@ -1,45 +1,35 @@
 import os
-import tkinter
+import cv2
 import pywhatkit
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
 
-def enviar_estudio():
-    print("Hola")
 
 def seleccionar_imagen():
-    archivo = filedialog.askopenfilename(filetypes=[("Archivos de imagen", "*.png")])
+    archivo = filedialog.askopenfilename(filetypes=[("Archivos de imagen", "*.jpg ")])
 
     if archivo is not None:
-        imagen = Image.open(archivo)
-        imagen = ImageTk.PhotoImage(imagen)
-        lbl_imagen = Label(root, image=imagen)
-        lbl_imagen.image = imagen
-        lbl_imagen.pack()
         path = os.path.abspath(archivo)
-        pywhatkit.sendwhats_image("+584123556285", path)
+        recorte = cv2.imread(path)
+        crop_img = recorte[0:910, 25:1120]
+        cv2.imwrite(path + "cropped.jpg", crop_img)
+        new_path = path + "cropped.jpg"
+        #cv2.imshow("cropped", crop_img)
+        numero = fieldNumber.get()
+        pywhatkit.sendwhats_image(numero, new_path)
+
 
 root = Tk()
+root.title("RX Sender")
 w = root.winfo_height()
-root.geometry("800x600")
 
-phone_number = tkinter.StringVar()
+label = Label(root, text="Ingrese el numero de telefono:")
+label.grid(row=0)
 
-label1 = Label(root, text="Hello world")
-label1.pack()
+button_image = Button(text="Seleccionar imagen y enviar", command=seleccionar_imagen)
+button_image.grid(row=2, padx=20, pady=10)
 
-imagen = PhotoImage(file="src/images/prueba.png")
-labelImagen = Label(image=imagen, width=100,height=100)
-labelImagen.pack()
-
-button_image = Button(text="Seleccionar Imagen", command=seleccionar_imagen)
-button_image.pack()
-
-fieldNumber = Entry(root, textvariable=phone_number)
-fieldNumber.pack()
-
-button = Button(text="Enviar Estudio", command=enviar_estudio())
-button.pack()
+fieldNumber = Entry(root)
+fieldNumber.grid(row=1,padx=20, pady=10)
 
 root.mainloop()
